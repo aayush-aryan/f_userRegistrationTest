f6bd946ce412e66321e8c8403ce8292e1ce9a59e
aayush-aryan <MY_NAME@example.com>
2021-01-23 02:57:13 +0530

    [aayush]added. a method to validate user last Name

--- src/main/java/org/bridgelabz/UserRegistration.java
+++ src/main/java/org/bridgelabz/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static Pattern FIRST_NAME_PATTERN = Pattern.compile("^[A-Z][a-z]{2,}$");
+    private static Pattern LAST_NAME_PATTERN = Pattern.compile("^[A-Z][a-z]{2,}$");
 
     boolean isValid = false;
     public boolean validateUserFirstName(String firstName) throws MyException {
@@ -15,4 +16,11 @@ public class UserRegistration {
         return isValid;
     }
 
+    public boolean validateUserLastName(String lastName) throws MyException {
+        isValid=LAST_NAME_PATTERN.matcher(lastName).matches();
+        if(!isValid){
+            throw new MyException("LastNameShouldStartsWithCapsLetter");
+        }
+        return isValid;
+    }
 }
